9feb67eff1491962f8884dc925fed8d50e0f73d0
Livenson-NICOLAS <nicolaslivenson3@gmail.com>
2025-12-19 15:03:53 -0500

    première ébauche

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,23 +1,16 @@
-<<<<<<< HEAD
-import CollaborateCta from "@/components/home/collaborate-cta";
-import EventBanner from "@/components/home/event-banner";
-import FeaturedProjects from "@/components/home/featured-projects";
-=======
->>>>>>> origin/main
 import Hero from "@/components/home/hero";
-import Highlights from "@/components/home/highlights";
+import EventBanner from "@/components/home/event-banner";
 import Kpis from "@/components/home/kpis";
-import Partners from "@/components/home/partners";
-<<<<<<< HEAD
+import Highlights from "@/components/home/highlights";
+import FeaturedProjects from "@/components/home/featured-projects";
 import PublicationsPreview from "@/components/home/publications-preview";
-=======
->>>>>>> origin/main
+import Partners from "@/components/home/partners";
+import CollaborateCta from "@/components/home/collaborate-cta";
 
 export default function Home() {
   return (
     <>
       <Hero />
-<<<<<<< HEAD
       <EventBanner />
       <Kpis />
       <Highlights />
@@ -25,11 +18,6 @@ export default function Home() {
       <PublicationsPreview />
       <Partners />
       <CollaborateCta />
-=======
-      <Kpis />
-      <Highlights />
-      <Partners />
->>>>>>> origin/main
     </>
   );
 }
