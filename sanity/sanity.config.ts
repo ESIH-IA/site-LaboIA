6047fa9e98cb28e661f7c4323e370b4cab3da35a
Livenson-NICOLAS <nicolaslivenson3@gmail.com>
2026-01-06 11:04:23 -0500

    fix(sanity): export schemaTypes array no

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
 
-import { schemaTypes } from "./schemaTypes";
+import schemaTypes from "./schemaTypes";
+
+
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "";
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
