e32884ce9aff76123978da192dfc66cc917ec57f
Livenson-NICOLAS <nicolaslivenson3@gmail.com>
2026-01-06 10:35:41 -0500

    adding sanity

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,37 +1,5 @@
-import { projectType } from "./project";
-import { publicationType } from "./publication";
-import { memberType } from "./member";
-import { partnerType } from "./partner";
-import { eventType } from "./event";
-import { newsType } from "./news";
-import { offerType } from "./offer";
-import { programType } from "./program";
-import { researchAxisType } from "./researchAxis";
-import { institutionalPageType } from "./institutionalPage";
-import { localeBlockContent, localeString, localeText } from "./locale";
-import { localeSlug } from "./localeSlug";
-import { resourceType } from "./resource";
-import { formSubmissionType } from "./formSubmission";
-import { kpiType } from "./kpi";
-import { labReportType } from "./labReport";
+import { type SchemaTypeDefinition } from 'sanity'
 
-export const schemaTypes = [
-  localeString,
-  localeText,
-  localeBlockContent,
-  localeSlug,
-  projectType,
-  publicationType,
-  memberType,
-  partnerType,
-  eventType,
-  newsType,
-  offerType,
-  programType,
-  researchAxisType,
-  resourceType,
-  formSubmissionType,
-  kpiType,
-  labReportType,
-  institutionalPageType,
-];
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [],
+}
